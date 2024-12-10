e913240bd25ff65c0b586a79f8f0ba61cee01e93
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-10 17:46:45 +0530

    managing & disabling data cache

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,6 +1,12 @@
 import Messages from '@/components/messages';
+import { unstable_noStore } from 'next/cache';
+
+// export const revalidate = 5;
+
+// export const dynamic = 'force-dynamic' // is equal to cache : 'no-store'
 
 export default async function MessagesPage() {
+  // unstable_noStore()  // Same as dynamic / cache : no-store
   const response = await fetch('http://localhost:8080/messages', {
 
     // next:{
