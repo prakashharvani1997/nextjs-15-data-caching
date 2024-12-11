3d15b3d23874e471be1948f6b45adaf3ae4aaea2
Prakash harvani <prakashharvani1997@gmail.com>
2024-12-11 10:50:55 +0530

    Setting up request memoization for custom data source

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,4 +1,5 @@
 import Messages from '@/components/messages';
+import { getMessages } from '@/lib/messages';
 import { unstable_noStore } from 'next/cache';
 
 // export const revalidate = 5;
@@ -7,11 +8,11 @@ export const dynamic = 'force-dynamic' // is equal to cache : 'no-store'
 
 export default async function MessagesPage() {
   // unstable_noStore()  // Same as dynamic / cache : no-store
-  const response = await fetch('http://localhost:8080/messages', {
+  // const response = await fetch('http://localhost:8080/messages', {
 
-    next:{
-      tags :['msg']
-    }
+  //   next:{
+  //     tags :['msg']
+  //   }
     // next:{
     //   revalidate : 5
     // }
@@ -22,8 +23,10 @@ export default async function MessagesPage() {
     // headers: {
     //   'X-ID': 'page',
     // },
-  });
-  const messages = await response.json();
+  // });
+  // const messages = await response.json();
+
+  const  messages = getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
